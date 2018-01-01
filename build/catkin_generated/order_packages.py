e10691980be4b164ad52f8170ad4ee97cdb1e621
dyson8910 <mech.b.dyson@gmail.com>
2018-01-01 12:00:31 +0900

    first commit

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/mech-user/dyson_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/mech-user/catkin_ws/devel;/opt/ros/kinetic".split(';') if "/home/mech-user/catkin_ws/devel;/opt/ros/kinetic" != "" else []
