e6c24559b7f4bc4fb8a0ebd826d046f79cb8fee1
dyson8910 <mech.b.dyson@gmail.com>
2018-01-10 07:30:05 +0900

    modified

--- src/handy/scripts/servo_controller.py
+++ src/handy/scripts/servo_controller.py
@@ -7,22 +7,22 @@ import pigpio
 pi = pigpio.pi()
 
 def shoulder_controller(msg):
-	pi.set_servo_pulsewidth()
+	pi.set_servo_pulsewidth(26,msg)
 
 def elbow_controller(msg):
-	pi.set_servo_pulsewidth()
+	pi.set_servo_pulsewidth(19,)
 
 def wrist_controller(msg):
-	pi.set_servo_pulsewidth()
+	pi.set_servo_pulsewidth(13)
 
 def finger_controller(msg):
-	pi.set_servo_pulsewidth()
+	pi.set_servo_pulsewidth(9,)
 
 rospy.init_node('servo_controller',anonymous=True)
-rospy.Subscriber("angle_shoulder",,shoulder_controller)
-rospy.Subscriber("angle_elbow",,elbow_controller)
-rospy.Subscriber("angle_wrist",,wrist_controller)
-rospy.Subscriber("angle_finger",,finger_controller)
+rospy.Subscriber("/angle_shoulder",,shoulder_controller)
+rospy.Subscriber("/angle_elbow",,elbow_controller)
+rospy.Subscriber("/angle_wrist",,wrist_controller)
+rospy.Subscriber("/angle_finger",,finger_controller)
 
 rospy.spin()
 
---
+++ src/handy/scripts/truck_controller.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import rospy
+import roslib
+import pigpio
+
+pi = pigpio.pi()
+
+right_red = 22
+right_black = 10
+left_red = 27
+left_black = 9
+
+pi.set_mode(right_red,pigpio.OUTPUT)
+pi.set_mode(right_black,pigpio.OUTPUT)
+pi.set_mode(left_red,pigpio.OUTPUT)
+pi.set_mode(left_black,pigpio.OUTPUT)
+
+pi.write(right_red,1)
+
+def left_wheel(msg):
+    if(msg == 1):
+        pi.write(left_red,1)
+        pi.write(left_black,0)
+    elif(msg == -1):
+        pi.write(left_red,0)
+        pi.write(left_black,1)
+    else:
+        pi.write(left_red,0)
+        pi.write(left_black,0)
+
+def right_wheel(msg):
+    if(msg == 1):
+        pi.write(right_red,1)
+        pi.write(right_black,0)
+    elif(msg == -1):
+        pi.write(right_red,0)
+        pi.write(right_black,1)
+    else:
+        pi.write(right_red,0)
+        pi.write(right_black,0)
+
+rospy.init_node('truck_controller'anonymous=True)
+rospy.Subscriber("/left_wheel",,left_wheel)
+rospy.Subscriber("/right_wheel",,right_wheel)
+
+rospy.spin()
